efb971080b22377354770ff023aa20dae3c405e7
PedroSolano27 <pedro_ssolano@yahoo.com>
2025-09-30 17:31:09 -0300

    Movido arquivos para um diretório a menos
    Também resolvido problema de renderização

---
+++ app/App.tsx
@@ -0,0 +1,23 @@
+// App principal
+
+// Terceiros
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Elementos
+import TaskListScreen from "./screens/TaskListScreen";
+import TaskFormScreen from "./screens/TaskFormScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <Stack.Navigator initialRouteName="List">
+            <Stack.Screen
+                name="List"
+                component={TaskListScreen}
+                options={{ title: "Tarefas" }}
+            />
+            <Stack.Screen name="Form" component={TaskFormScreen} />
+        </Stack.Navigator>
+    );
+}
